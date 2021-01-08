b63d55e05bdf1535157ab4f85bb558e34371907e
Jorge Costa <nando.costa_martins@yahoo.com.br>
2021-01-08 11:03:51 -0300

    Testando Novamente

--- src/teste1/teste.java
+++ src/teste1/teste.java
@@ -4,6 +4,21 @@ public class teste {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		
+		System.out.println("rgaergwerg");
+		System.out.println("ergergro");
+		System.out.println("erg");
+		System.out.println("Hello");
+		
+		
+		
+		System.out.println("Hello");
+		System.out.println("ergrgqerqgo");
+		System.out.println("Hello");
+		System.out.println("Hergqergqero");
+		 
+		
+		
 	}
 
 }
